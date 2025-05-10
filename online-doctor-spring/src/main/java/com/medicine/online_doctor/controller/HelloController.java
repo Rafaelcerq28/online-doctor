395b2b9bf4c2a3f49381aa1b52c329722f85c052
Rafaelcerq28 <rafaelcerq28@gmail.com>
2025-05-10 17:28:32 +0100

    adding exceptions

--- online-doctor-spring/src/main/java/com/medicine/online_doctor/controller/HelloController.java
+++ online-doctor-spring/src/main/java/com/medicine/online_doctor/controller/HelloController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medicine.online_doctor.DTO.AuthenticationDTO;
+import com.medicine.online_doctor.exeptions.CustomizedResponseEntityExceptionHandler;
+import com.medicine.online_doctor.exeptions.UserNotFoundException;
 import com.medicine.online_doctor.model.LoginDetails;
 import com.medicine.online_doctor.service.HelloService;
 
@@ -18,9 +20,14 @@ public class HelloController {
     HelloService helloService;
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Doctor!";
+    public ResponseEntity<String> hello() {
+
+        if(true){
+            throw new UserNotFoundException("User not found");      }
+
+        return ResponseEntity.ok("Hello World!");
     }
+    
 
 
 
